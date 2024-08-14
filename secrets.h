7d84e6d9563928ebb3e1dd002e78e02f766449fc
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2024-08-14 22:40:14 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -4,28 +4,16 @@
 // --> REPLACE BY YOUR VALUES <--
 //-------------------------------
 
-// application identifier - pre-LoRaWAN 1.1.0, this was called appEUI
-// when adding new end device in TTN, you will have to enter this number
-// you can pick any number you want, but it has to be unique
-uint64_t joinEUI = 0x12AD1011B0C0FFEE;
+// JoinEUI - previous versions of LoRaWAN called this AppEUI
+// for development purposes you can use all zeros - see wiki for details
+#define RADIOLIB_LORAWAN_JOIN_EUI  0x0000000000000000
 
-// device identifier - this number can be anything
-// when adding new end device in TTN, you can generate this number,
-// or you can set any value you want, provided it is also unique
-uint64_t devEUI = 0x70B3D57ED005E120;
+// The Device EUI & two keys can be generated on the TTN console 
+// Replace with your Device EUI
+#define RADIOLIB_LORAWAN_DEV_EUI   0x---------------
 
-// select some encryption keys which will be used to secure the communication
-// there are two of them - network key and application key
-// because LoRaWAN uses AES-128, the key MUST be 16 bytes (or characters) long
+// Replace with your App Key 
+#define RADIOLIB_LORAWAN_APP_KEY   0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x-- 
 
-// network key is the ASCII string "topSecretKey1234"
-uint8_t nwkKey[] = { 0x74, 0x6F, 0x70, 0x53, 0x65, 0x63, 0x72, 0x65,
-                     0x74, 0x4B, 0x65, 0x79, 0x31, 0x32, 0x33, 0x34 };
-
-// application key is the ASCII string "aDifferentKeyABC"
-uint8_t appKey[] = { 0x61, 0x44, 0x69, 0x66, 0x66, 0x65, 0x72, 0x65,
-                     0x6E, 0x74, 0x4B, 0x65, 0x79, 0x41, 0x42, 0x43 };
-
-// prior to LoRaWAN 1.1.0, only a single "nwkKey" is used
-// when connecting to LoRaWAN 1.0 network, "appKey" will be disregarded
-// and can be set to NULL
+// Put your Nwk Key here
+#define RADIOLIB_LORAWAN_NWK_KEY   0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--, 0x--
