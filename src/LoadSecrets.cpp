7b685a676e251592f54a2188afa6f71f0849d579
Matthias Prinke <m.prinke@arcor.de>
2025-03-08 17:26:58 +0100

    Refactoring

--- src/LoadSecrets.cpp
+++ src/LoadSecrets.cpp
@@ -32,6 +32,7 @@
 // History:
 //
 // 20240815 Copied from BresserWeatherSensorLW
+// 20240928 Refactoring & modification for LoRaWAN v1.0.4 (requires no nwkKey)
 //
 // ToDo:
 // -
@@ -41,7 +42,7 @@
 #include "LoadSecrets.h"
 
 // Load LoRaWAN secrets from file 'secrets.json' on LittleFS, if available
-void loadSecrets(uint64_t &joinEUI, uint64_t &devEUI, uint8_t *nwkKey, uint8_t *appKey)
+void loadSecrets(bool requireNwkKey, uint64_t &joinEUI, uint64_t &devEUI, uint8_t *nwkKey, uint8_t *appKey)
 {
 
   if (!LittleFS.begin(
@@ -51,36 +52,38 @@ void loadSecrets(uint64_t &joinEUI, uint64_t &devEUI, uint8_t *nwkKey, uint8_t *
 #endif
           ))
   {
-    log_d("Could not initialize LittleFS.");
+    log_e("Could not initialize LittleFS.");
+    return;
   }
-  else
-  {
+
     File file = LittleFS.open("/secrets.json", "r");
 
     if (!file)
     {
       log_i("File 'secrets.json' not found.");
+    return;
     }
-    else
-    {
+
       log_d("Reading 'secrets.json'");
       JsonDocument doc;
 
       // Deserialize the JSON document
       DeserializationError error = deserializeJson(doc, file);
+  file.close();
+
       if (error)
       {
-        log_d("Failed to read JSON file, using defaults.");
+    log_e("Failed to read JSON file, using defaults.");
+    return;
       }
-      else
-      {
+
         const char *joinEUIStr = doc["joinEUI"];
         if (joinEUIStr == nullptr)
         {
           log_e("Missing joinEUI.");
-          file.close();
           return;
         }
+
         uint64_t _joinEUI = 0;
         for (int i = 2; i < 18; i += 2)
         {
@@ -97,9 +100,9 @@ void loadSecrets(uint64_t &joinEUI, uint64_t &devEUI, uint8_t *nwkKey, uint8_t *
         if (devEUIStr == nullptr)
         {
           log_e("Missing devEUI.");
-          file.close();
           return;
         }
+
         uint64_t _devEUI = 0;
         for (int i = 2; i < 18; i += 2)
         {
@@ -112,7 +115,6 @@ void loadSecrets(uint64_t &joinEUI, uint64_t &devEUI, uint8_t *nwkKey, uint8_t *
         if (_devEUI == 0)
         {
           log_e("devEUI is zero.");
-          file.close();
           return;
         }
         // printf() cannot print 64-bit hex numbers (sic!), so we split it in two 32-bit numbers...
@@ -121,39 +123,6 @@ void loadSecrets(uint64_t &joinEUI, uint64_t &devEUI, uint8_t *nwkKey, uint8_t *
         uint8_t check = 0;
         bool fail = false;
 
-        log_d("nwkKey:");
-        uint8_t _nwkKey[16];
-        for (size_t i = 0; i < 16; i++)
-        {
-          const char *buf = doc["nwkKey"][i];
-          if (buf == nullptr)
-          {
-            fail = true;
-            break;
-          }
-          unsigned int tmp;
-          sscanf(buf, "%x", &tmp);
-          _nwkKey[i] = static_cast<uint8_t>(tmp);
-          check |= _nwkKey[i];
-#if CORE_DEBUG_LEVEL >= ARDUHAL_LOG_LEVEL_INFO
-          printf("0x%02X", _nwkKey[i]);
-          if (i < 15)
-          {
-            printf(", ");
-          }
-#endif
-        } // for all nwk_key bytes
-#if CORE_DEBUG_LEVEL >= ARDUHAL_LOG_LEVEL_INFO
-        printf("\n");
-#endif
-        if (fail || (check == 0))
-        {
-          log_e("nwkKey parse error");
-          file.close();
-          return;
-        }
-
-        check = 0;
         log_i("appKey:");
         uint8_t _appKey[16];
         for (size_t i = 0; i < 16; i++)
@@ -182,17 +151,49 @@ void loadSecrets(uint64_t &joinEUI, uint64_t &devEUI, uint8_t *nwkKey, uint8_t *
         if (fail || (check == 0))
         {
           log_e("appKey parse error");
-          file.close();
           return;
         }
+
+  check = 0;
+  if (requireNwkKey)
+  {
+    log_d("nwkKey:");
+    uint8_t _nwkKey[16];
+    for (size_t i = 0; i < 16; i++)
+    {
+      const char *buf = doc["nwkKey"][i];
+      if (buf == nullptr)
+      {
+        fail = true;
+        break;
+      }
+      unsigned int tmp;
+      sscanf(buf, "%x", &tmp);
+      _nwkKey[i] = static_cast<uint8_t>(tmp);
+      check |= _nwkKey[i];
+#if CORE_DEBUG_LEVEL >= ARDUHAL_LOG_LEVEL_INFO
+      printf("0x%02X", _nwkKey[i]);
+      if (i < 15)
+      {
+        printf(", ");
+      }
+#endif
+    } // for all nwk_key bytes
+#if CORE_DEBUG_LEVEL >= ARDUHAL_LOG_LEVEL_INFO
+    printf("\n");
+#endif
+    if (fail || (check == 0))
+    {
+      log_e("nwkKey parse error");
+      return;
+    }
+    memcpy(nwkKey, _nwkKey, 16);
+  } // if (requireNwkKey)
 
         // Every check passed, copy intermediate values as result
         joinEUI = _joinEUI;
         devEUI = _devEUI;
-        memcpy(nwkKey, _nwkKey, 16);
         memcpy(appKey, _appKey, 16);
-      } // deserializeJson o.k.
-    } // file read o.k.
-    file.close();
-  } // LittleFS o.k.
+
+  return;
 }
