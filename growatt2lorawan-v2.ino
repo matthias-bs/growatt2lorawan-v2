c0ab6a5fb9cde4244b9d30d6781a68dc0ef85a25
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2026-01-30 09:33:55 +0100

    Update RadioLib version and improve SPI configuration

--- growatt2lorawan-v2.ino
+++ growatt2lorawan-v2.ino
@@ -17,7 +17,7 @@
 //
 // Library dependencies (tested versions):
 // ---------------------------------------
-// RadioLib                             7.4.0
+// RadioLib                             7.5.0
 // LoRa_Serialization                   3.3.1
 // ESP32Time                            2.0.6
 //
@@ -64,6 +64,7 @@
 //          Added implementation for using Lilygo T3S3 SX1262/SX1276/LR1121
 // 20250622 Updated to RadioLib v7.2.0
 // 20251028 Updated to RadioLib v7.4.0
+// 20260130 Fixed radio module initialization for LilyGo T3S3 boards using RadioLib 7.5.0
 //
 //
 // Notes:
@@ -120,11 +121,16 @@ struct sPrefs
 } prefs;
 
 
+#if defined(ARDUINO_LILYGO_T3S3_SX1262) || defined(ARDUINO_LILYGO_T3S3_SX1276) || defined(ARDUINO_LILYGO_T3S3_LR1121)
+static SPIClass *spi = new SPIClass(SPI);
+
+// Create radio object with custom SPI configuration
+LORA_CHIP radio = new Module(PIN_LORA_NSS, PIN_LORA_IRQ, PIN_LORA_RST, PIN_LORA_GPIO, *spi);
+
+#else
 // Create radio object
 LORA_CHIP radio = new Module(PIN_LORA_NSS, PIN_LORA_IRQ, PIN_LORA_RST, PIN_LORA_GPIO);
 
-#if defined(ARDUINO_LILYGO_T3S3_SX1262) || defined(ARDUINO_LILYGO_T3S3_SX1276) || defined(ARDUINO_LILYGO_T3S3_LR1121)
-SPIClass *spi = nullptr;
 #endif
 
 #if defined(ARDUINO_LILYGO_T3S3_LR1121)
@@ -572,14 +578,10 @@ void setup()
   int16_t state = 0; // return value for calls to RadioLib
 
   #if defined(ARDUINO_LILYGO_T3S3_SX1262) || defined(ARDUINO_LILYGO_T3S3_SX1276) || defined(ARDUINO_LILYGO_T3S3_LR1121)
-  // Use local radio object with custom SPI configuration
-  spi = new SPIClass(SPI);
-  spi->begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
-  radio = new Module(PIN_LORA_NSS, PIN_LORA_IRQ, PIN_LORA_RST, PIN_LORA_GPIO, *spi);
-  #elif defined(ARDUINO_CUBECELL_BOARD)
-  SX1262 radio = new Module(RADIOLIB_BUILTIN_MODULE);
+    // Radio object with custom SPI configuration
+    spi->begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
   #endif
-
+  
   // setup the radio based on the pinmap (connections) in config.h
   radio.reset();
   LoRaWANNode node(&radio, &Region, subBand);
